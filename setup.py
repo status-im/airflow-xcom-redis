e307d97bc6e0e79ab6eff62ef7f65d78cc37e86a
Jakub Sokołowski <jakub@status.im>
2021-03-30 18:03:21 +0200

    change version to 0.1.0
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'),
 setup(
     # Basic project information
     name='airflow-xcom-redis',
-    version='0.0.1',
+    version='0.1.0',
     # Authorship and online reference
     author='Jakub Sokołowski',
     author_email='jakub@status.im',
